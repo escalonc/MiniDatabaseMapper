162604ca39785102b9d985e01105d65323f456cf
dessire <dessireochoa12@gmail.com>
2023-09-15 21:43:53 -0600

    Table

--- SqlBuilder/DatabaseContext.cs
+++ SqlBuilder/DatabaseContext.cs
@@ -14,25 +14,64 @@ namespace SqlBuilder
 
         public void Initialize()
         {
-            var type = typeof(ISqlTable);
-            var types = AppDomain.CurrentDomain.GetAssemblies()
-                .SelectMany(s => s.GetTypes())
-                .Where(p => type.IsAssignableFrom(p));
-
-            foreach (var tableType in types)
+            using (var connection = new SqliteConnection(ConnectionString))
             {
-                var tableName = tableType.Name;
+                connection.Open();
 
-                var columnProperties = tableType.GetProperties();
+                var type = typeof(ISqlTable);
+                var types = AppDomain.CurrentDomain.GetAssemblies()
+                    .SelectMany(s => s.GetTypes())
+                    .Where(p => type.IsAssignableFrom(p) && p != type);
 
-                foreach (var property in columnProperties)
+                foreach (var tableType in types)
                 {
+                    var tableName = tableType.Name;
+
+                    var columnProperties = tableType.GetProperties();
+                    var columns = new List<string>();
+
+                    foreach (var property in columnProperties)
+                    {
+                        var columnName = property.Name;
+                        var columnType = GetSqlTypeForProperty(property.PropertyType);
+
                         // check types and build sql fields
+                        columns.Add($"{columnName} {columnType}");
+                    }
+
+                     // concatenate fields with create statement
+                    var createTableSql = $"CREATE TABLE IF NOT EXISTS {tableName} ({string.Join(", ", columns)})";
+
+                    //execute query
+                    //using (var command = new SqliteCommand(createTableSql, connection))
+                    //{
+                    //  command.ExecuteNonQuery();
+                    //}
                 }
-                
-                // concatenate fields with create statement
-                
-                //execute query
+            }
+        }
+
+        private string GetSqlTypeForProperty(Type propertyType)
+        {
+            if (propertyType == typeof(string))
+            {
+                return "TEXT";
+            }
+            else if (propertyType == typeof(int))
+            {
+                return "INTEGER";
+            }
+            else if (propertyType == typeof(decimal))
+            {
+                return "NUMERIC";
+            }
+            else if (propertyType == typeof(bool))
+            {
+                return "BOOLEAN";
+            }
+            else
+            {
+                throw new ArgumentException($"Unsupported property type: {propertyType.Name}");
             }
         }
         
