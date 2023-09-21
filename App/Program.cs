145eb3acb174804df122a099a6ae2bf98d919906
Dessire1012 <dessire@unitec.edu>
2023-09-21 08:51:03 -0600

    Program.cs más ordenado con el crud

--- App/Program.cs
+++ App/Program.cs
@@ -5,17 +5,28 @@ var db = new Database();
 
 db.Initialize();
 
-// db.Toys.Create(new Toy { Name = "Osito", Price = 444 });
-db.Toys.Delete(x => x.Price == 600);
+//Create
+db.Toys.Create(new Toy { Name = "Osito", Price = 444 });
+db.Toys.Create(new Toy { Name = "Carrito", Price = 50 });
+db.Toys.Create(new Toy { Name = "Muñeca", Price = 100 });
+db.Toys.Create(new Toy { Name = "Pony", Price = 250 });
+db.Toys.Create(new Toy { Name = "Peluche", Price = 60 });
+
+//Read
 var toys = db.Toys.Find(x => x.Id == 0);
 
-// foreach (var toy in toys)
-// {
-//     Console.WriteLine($"Name: {toy.Name}, Price{toy.Price}");
-// }
-//
+foreach (var toy in toys)
+ {
+     Console.WriteLine($"Name: {toy.Name}, Price{toy.Price}");
+ }
+
+//Update
 db.Toys.Update(new
 {
     Price = 600
 }, t => t.Price == 500);
+
+//Delete
+db.Toys.Delete(x => x.Price == 600);
+
 Console.WriteLine("Hello, World!");
